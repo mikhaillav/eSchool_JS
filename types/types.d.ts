08f965c94771fe256533d428ea5996c25f0c651d
mikhaillav <mikhaillavelin@gmail.com>
2023-10-02 18:38:16 +0300

    remove husky

---
+++ types/types.d.ts
@@ -0,0 +1,46 @@
+declare type profile = {
+	firstName: string;
+	lastName: string;
+	middleName: string;
+	birthDate: number;
+	phoneHome: string;
+	phoneMob: string;
+	email: string;
+};
+
+declare type device = {
+	prsId: number;
+	pdId: number;
+	cliType: "web" | string;
+	cliVer: string;
+	cliOs: string;
+	cliOsVer: string;
+	deviceName: string;
+	deviceModel: string;
+	pushIsUserAllow: number;
+	pushToken: string;
+	isNew: boolean;
+	dbSessionId: number;
+};
+
+declare type currentPosition = {
+	prsId: number;
+	prsFio: string;
+	userId: number;
+	orgId: number;
+	orgName: string;
+	orgFullName: string;
+	posId: number;
+	posName: string;
+	activeSessionId: number;
+	isOrgAdmin: boolean;
+	codeLang: "eng";
+	iso2: "en";
+	idLogo: number;
+	lastChanged: number;
+	posTypeCode: string;
+	orgnum: string;
+	baseUrl: string;
+	isReady: number;
+	orgYearId: number;
+};
